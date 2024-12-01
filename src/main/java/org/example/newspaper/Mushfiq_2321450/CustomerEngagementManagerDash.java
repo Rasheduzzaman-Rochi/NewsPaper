8c2b44ce6231b61139aab102ea4b15ade3adecda
Mushfiq <mushfiqurrahman057@gmail.com>
2024-12-01 11:44:50 +0600

    CustomerEngagementManagerDash.fxml, CustomerEngagementManagerDash.java

---
+++ src/main/java/org/example/newspaper/Mushfiq_2321450/CustomerEngagementManagerDash.java
@@ -0,0 +1,75 @@
+package org.example.newspaper.Mushfiq_2321450;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class CustomerEngagementManagerDash {
+    @javafx.fxml.FXML
+    private Label label1;
+    @javafx.fxml.FXML
+    private Label label2;
+
+    @javafx.fxml.FXML
+    public void OnFeedbackManagementButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnContentManagementButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnTrainingManagementButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnAnalyticsButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnGatherFeedbackButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnDesignSubscriptionModelButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnLogoutButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnCreateSurveyButtonClick(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void OnPromoteSubscriptionButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void OnCreateSurveyButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnGatherFeedbackButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnDesignSubscriptionModelButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnPromoteSubscriptionButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnContentManagementButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnTrainingManagementButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnFeedbackManagementButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnAnalyticsButtonClick(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void OnLogoutButtonClick(javafx.event.ActionEvent actionEvent) {
+
+    }
+}
