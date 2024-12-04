9beec2c42eb63713db9733cf000715895a5ca4e7
Mushfiq <mushfiqurrahman057@gmail.com>
2024-12-04 22:18:59 +0600

    Added codes on LayoutDesignerDash for scenes switching

--- src/main/java/org/example/newspaper/Mushfiq_2321450/LayoutDesignerDash.java
+++ src/main/java/org/example/newspaper/Mushfiq_2321450/LayoutDesignerDash.java
@@ -2,7 +2,15 @@ package org.example.newspaper.Mushfiq_2321450;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.EventObject;
 
 public class LayoutDesignerDash {
 
@@ -13,47 +21,129 @@ public class LayoutDesignerDash {
     private Label layoutDesigner;
 
     @FXML
-    void OnEditArticleButtonClick(ActionEvent event) {
+    public void OnEditArticleButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner3.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    void OnEditHeadlinesButtonClick(ActionEvent event) {
+    public void OnEditHeadlinesButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner2.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    void OnEditPageAdvertisementsButtonClick(ActionEvent event) {
+    void OnEditPageAdvertisementsButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner4.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    void OnEditPageLayoutButtonClick(ActionEvent event) {
+    void OnEditPageLayoutButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner1.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    void OnLogoutButtonClick(ActionEvent event) {
+    void OnLogoutButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     @FXML
-    void OnReviewLayoutsButtonClick(ActionEvent event) {
+    void OnReviewLayoutsButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner6.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    void OnSelectBestArticleButtonClick(ActionEvent event) {
+    void OnSelectBestArticleButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner5.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    void OnSelectPageResolutionButtonClick(ActionEvent event) {
+    void OnSelectPageResolutionButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner7.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @FXML
-    void OnSelectPaperTypeButtonClick(ActionEvent event) {
+    void OnSelectPaperTypeButtonClick(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LayoutDesigner8.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
