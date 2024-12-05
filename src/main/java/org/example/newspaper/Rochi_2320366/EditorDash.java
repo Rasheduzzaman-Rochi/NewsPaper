62ce230121a401a27e145679323cd84e1ed46242
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-12-06 00:58:44 +0600

    Scene switching code added on onEditHeadlines() and onLogout in EditorDash

--- src/main/java/org/example/newspaper/Rochi_2320366/EditorDash.java
+++ src/main/java/org/example/newspaper/Rochi_2320366/EditorDash.java
@@ -6,12 +6,26 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.newspaper.LoginApplication;
 
 import java.io.IOException;
 
 public class EditorDash {
     @javafx.fxml.FXML
     public void onTrack(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("Editor8.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void onEdit(ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("Editor1.fxml"));
             Scene scene = new Scene(root);
@@ -24,7 +38,7 @@ public class EditorDash {
     }
 
     @javafx.fxml.FXML
-    public void onEdit(ActionEvent actionEvent) {
+    public void onEditHeadlines(ActionEvent actionEvent) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("Editor2.fxml"));
             Scene scene = new Scene(root);
@@ -41,23 +55,6 @@ public class EditorDash {
     }
 
     @javafx.fxml.FXML
-    public void onEditHeadlines(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void onLogout(ActionEvent actionEvent) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @javafx.fxml.FXML
     public void onManageDeadlines(ActionEvent actionEvent) {
     }
 
@@ -71,5 +68,15 @@ public class EditorDash {
 
     @javafx.fxml.FXML
     public void onCoordinate(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onLogout(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
