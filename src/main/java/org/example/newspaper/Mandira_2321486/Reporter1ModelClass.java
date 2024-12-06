0d181c21cb84dabdf0446ad997299b6d3cd7470e
Mandira <mandirapaul3002@gmail.com>
2024-12-06 20:46:26 +0600

    update Model Class in Reporter1

---
+++ src/main/java/org/example/newspaper/Mandira_2321486/Reporter1ModelClass.java
@@ -0,0 +1,48 @@
+package org.example.newspaper.Mandira_2321486;
+
+import java.time.LocalDate;
+
+public class Reporter1ModelClass {
+    String title;
+    Integer id;
+    LocalDate date;
+
+    public Reporter1ModelClass(String title, Integer id, LocalDate date) {
+        this.title = title;
+        this.id = id;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Reporter1ModelClass{" +
+                "title='" + title + '\'' +
+                ", id=" + id +
+                ", date=" + date +
+                '}';
+    }
+}
