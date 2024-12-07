70ca5c9cc8f4958004c33576a1f4991b185112f6
Mandira <mandirapaul3002@gmail.com>
2024-12-08 01:10:21 +0600

    Add new table and button in scenebuilder design

--- src/main/java/org/example/newspaper/Mandira_2321486/Reporter2.java
+++ src/main/java/org/example/newspaper/Mandira_2321486/Reporter2.java
@@ -5,38 +5,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Reporter2 {
-    @javafx.fxml.FXML
-    private TextArea notesTextArea;
-    @javafx.fxml.FXML
-    private TextField nameBox;
-    @javafx.fxml.FXML
-    private TextArea feedbackNotesTextArea;
+
     @javafx.fxml.FXML
     private ComboBox categoryComboBox;
     @javafx.fxml.FXML
     private TextArea contentTextArea;
     @javafx.fxml.FXML
-    private TextField titleBox;
+    private TextField titleTextField;
+    @javafx.fxml.FXML
+    private TableColumn titleCol;
+    @javafx.fxml.FXML
+    private TextField subTitleTextField;
+    @javafx.fxml.FXML
+    private TableColumn categoryCol;
+    @javafx.fxml.FXML
+    private TableColumn subTitleCol;
+    @javafx.fxml.FXML
+    private TableColumn articleIDCol;
+    @javafx.fxml.FXML
+    private ComboBox filterCategoryComboBox;
+    @javafx.fxml.FXML
+    private TableView table;
+    @javafx.fxml.FXML
+    private TextField articleIDTextField;
 
     @javafx.fxml.FXML
     public void submitButton(ActionEvent actionEvent) {
-    }
-
-
-    @javafx.fxml.FXML
-    public void notesSaveButton(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void reviewButton(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
@@ -50,5 +50,13 @@ public class Reporter2 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @javafx.fxml.FXML
+    public void showButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
     }
 }
